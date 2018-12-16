ff9d5f4bbb705aa6418861f03d62d62132fac485
xblanc33 <blancxav@gmail.com>
2018-12-16 23:55:54 +0100

    crossEntropy bug fix

--- cartographer/EntropyCampaignManager.js
+++ cartographer/EntropyCampaignManager.js
@@ -26,12 +26,12 @@ class EntropyCampaignManager {
             if (index > 0) {
                 let previousFrom = Math.max(0,index - this.deepth);
                 for (let previousIndex = previousFrom; previousIndex < index; previousIndex++) {
-                    previousEventSeq[previousIndex-previousFrom] = expedition.events[previousIndex];
+                    previousEventSeq.push(expedition.events[previousIndex]);
                 }
             }
             probabilitySum = probabilitySum + Math.log2(this.computeProbability(previousEventSeq, nextItem));
         }
-        return probabilitySum / expedition.events.length;
+        return -(probabilitySum / expedition.events.length);
     }
 
     computeProbability(previousEventSeq, nextItem) {
@@ -42,13 +42,14 @@ class EntropyCampaignManager {
 
     updateModel(expedition) {
         if (expedition.campaignId !== this.campaignId) throw "EntropyCampaignManager received wrong expedition";
+        logger.info(`update model with expedition ${JSON.stringify(expedition.events)}`)
         for (let index = 0; index < expedition.events.length; index++) {
             let nextItem = expedition.events[index];
             let previousEventSeq = [];
             if (index > 0) {
                 let previousFrom = Math.max(0,index - this.deepth);
                 for (let previousIndex = previousFrom; previousIndex < index; previousIndex++) {
-                    previousEventSeq[previousIndex-previousFrom] = expedition.events[previousIndex];
+                    previousEventSeq.push(expedition.events[previousIndex]);
                 }
             }
             this.updateProbability(previousEventSeq, nextItem);
@@ -56,7 +57,9 @@ class EntropyCampaignManager {
     }
 
     updateProbability(previousEventSeq, nextItem) {
+        logger.info(`update ${JSON.stringify(previousEventSeq)} and ${JSON.stringify(nextItem)}`)
         let hashPreviousEventSeq = hashNGram(previousEventSeq);
+        logger.info(`hashPreviousEventSeq: ${hashPreviousEventSeq}`);
         let ngram = this.ngramMap.get(hashPreviousEventSeq);
         if (ngram === undefined) {
             ngram = new Ngram(hashPreviousEventSeq);
@@ -73,13 +76,18 @@ function hashItem(event) {
 }
 
 function hashNGram(eventSeq) {
-    return hashCode(eventSeq.map( event => {hashItem(event)}).join());
+    return hashCode(eventSeq.map( event => {
+        return event.type+event.selector;
+    }).join(','));
 }
 
 function hashCode(s) {
-    let h;
-    for(let i = 0; i < s.length; i++) 
-          h = Math.imul(31, h) + s.charCodeAt(i) | 0;
-
-    return h;
-}
+    let hash = 0, i, chr;
+    if (s.length === 0) return hash;
+    for (i = 0; i < s.length; i++) {
+      chr   = s.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
--- cartographer/Ngram.js
+++ cartographer/Ngram.js
@@ -1,13 +1,29 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [new winston.transports.Console(),],
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    )
+});
+
 class Ngram {
     constructor(hash) {
         this.hash = hash;
         this.successorMap = new Map();
         this.size = 0;
+        logger.info(`A new Ngram has been created`);
     }
 
     getSuccessorProbability(eventHash) {
+        let probability;
         let occurence = this.successorMap.get(eventHash);
-        return occurence === undefined ? 0 : occurence / this.size;
+        if (occurence === undefined) probability = 0;
+        if (occurence !== undefined) probability = occurence / this.size;
+        logger.info(`Probability for ${eventHash} is ${probability}`);
+        return probability;
     }
 
     updateSuccessorOccurence(eventHash) {
@@ -15,6 +31,7 @@ class Ngram {
         occurence === undefined ? occurence = 1 : occurence++ ;
         this.successorMap.set(eventHash, occurence);
         this.size++;
+        logger.info(`Update model for ${eventHash} with ${occurence} / ${this.size}`);
     }
 }
 
